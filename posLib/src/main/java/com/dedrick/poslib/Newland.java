20efd771becbe6bdef4745eb9c4b5b47d467fff1
henock.tshibanda <multshibanda@gmail.com>
2024-07-25 15:53:09 +0100

    Feat: Add NSDK libs

--- posLib/src/main/java/com/dedrick/poslib/Newland.java
+++ posLib/src/main/java/com/dedrick/poslib/Newland.java
@@ -10,6 +10,7 @@ public class Newland extends POSPrinter {
     @Override
     protected void print(String bank, String bankingAgent, String activityPoint, String address, String transactionType, String account_number, String stan, String created_at, String currency, String main_amount, String description, String terminalId) {
         super.print(bank, bankingAgent, activityPoint, address, transactionType, account_number, stan, created_at, currency, main_amount, description, terminalId);
+
         System.out.println("Printing on Newland");
     }
 }
