34e928d4841315294b892f7e491ad8db964a927e
henock.tshibanda <multshibanda@gmail.com>
2024-09-30 11:44:43 +0100

    Fix: few fixes

--- posLib/src/main/java/com/dedrick/poslib/PrintingActivity.java
+++ posLib/src/main/java/com/dedrick/poslib/PrintingActivity.java
@@ -2,12 +2,8 @@ package com.dedrick.poslib;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.dedrick.utilities.PrinterUtil;
-import com.dedrick.utilities.ServiceHelper;
 
 public class PrintingActivity extends AppCompatActivity {
     @Override
@@ -30,15 +26,15 @@ public class PrintingActivity extends AppCompatActivity {
             case "F20":
                 posPrinter = new F20(getApplication());
                 break;
-            case "NL":
-                posPrinter = new Newland(getApplication());
+            case "NL950":
+                posPrinter = new Newland950(getApplication());
                 break;
         }
 
         if (posPrinter != null) {
             posPrinter.print(intent.getStringExtra("bank"), intent.getStringExtra("bankingAgent"), intent.getStringExtra("activityPoint"), intent.getStringExtra("address"), intent.getStringExtra("transactionType"), intent.getStringExtra("account_number"), intent.getStringExtra("stan"), intent.getStringExtra("created_at"), intent.getStringExtra("currency"), intent.getStringExtra("main_amount"), intent.getStringExtra("description"), intent.getStringExtra("terminalId"));
         }else{
-            System.out.println("POS Printer not null");
+            System.out.println("POS Printer is null");
         }
 
     }
